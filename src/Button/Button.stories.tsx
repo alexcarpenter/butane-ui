0f7df35207fabb89f1a42bb267a553cab9a58872
Alex Carpenter <alex.carpenter@nationbuilder.com>
2020-07-09 18:45:23 -0400

    Button styling

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -3,10 +3,12 @@ import { action } from '@storybook/addon-actions';
 import { Button } from './';
 
 export default {
-  title: 'Button',
-  parameters: {
-    info: { inline: true },
-  },
+  title: 'Button'
 };
 
-export const Default = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Primary = () => <Button onClick={action('clicked')}>Primary Button</Button>;
+export const Secondary = () => <Button variant="secondary" onClick={action('clicked')}>Secondary Button</Button>;
+export const Neutral = () => <Button variant="neutral" onClick={action('clicked')}>Neutral Button</Button>;
+export const Disabled = () => <Button disabled onClick={action('clicked')}>Disabled Button</Button>;
+export const Outlined = () => <Button outlined onClick={action('clicked')}>Outlined Button</Button>;
+export const Link = () => <Button as="a" href="#" onClick={action('clicked')}>Link Button</Button>;
